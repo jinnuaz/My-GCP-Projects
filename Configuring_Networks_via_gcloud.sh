03fbcbae9e8847139fa1e3e460c53d492d1986e4
myWorkspace537 <shrutifrancis.ai@gmail.com>
2024-04-08 22:41:26 -0700

    commited

--- Configuring_Networks_via_gcloud.sh
+++ Configuring_Networks_via_gcloud.sh
@@ -1,8 +1,18 @@
 # (Optional) You can list the active account name with this command:
-gcloud auth list
+#gcloud auth list
 
 #You can list the project ID with this command:
-gcloud config list
+#gcloud config list
 
 # Instrall pinf to use later in this lab
-sudo apt install iputils-ping
+#sudo apt install iputils-ping
+
+
+# Creating a custom VPC labnet
+gcloud compute networks create labnet --subnet-mode=
+
+# Create a subnet for labnet
+gcloud compute networks subnets create labnet-sub --network labnet --region "us-west1" --range 10.0.0.0/28
+
+#List the network in the project
+gcloud compute networks list
