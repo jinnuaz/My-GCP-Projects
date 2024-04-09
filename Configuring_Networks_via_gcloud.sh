dcab435a3003e6c4cc91c058699139fa568d5ca1
myWorkspace537 <shrutifrancis.ai@gmail.com>
2024-04-08 22:42:44 -0700

    commited

--- Configuring_Networks_via_gcloud.sh
+++ Configuring_Networks_via_gcloud.sh
@@ -9,7 +9,7 @@
 
 
 # Creating a custom VPC labnet
-gcloud compute networks create labnet --subnet-mode=
+gcloud compute networks create labnet --subnet-mode=custom
 
 # Create a subnet for labnet
 gcloud compute networks subnets create labnet-sub --network labnet --region "us-west1" --range 10.0.0.0/28
