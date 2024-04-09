c196286377cfd7a0aede408adfbcfe8ebd428846
myWorkspace537 <shrutifrancis.ai@gmail.com>
2024-04-09 14:57:20 -0700

    commited

--- BigQuery_Soccer_Data_Ingestion.sh
+++ BigQuery_Soccer_Data_Ingestion.sh
@@ -2,4 +2,5 @@ bq --location=US mk -d \
     --description "This is my dataset." \
     soccar
 
-bq --location=US mk -d --default_table_expiration 3600 --description "This is my dataset." mydataset
+bq --location=US mk -d --description "This is my dataset." mydataset
+bq mk --table mydataset.mytable /tmp/myschema.json
